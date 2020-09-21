8b7e58563491b38eed483d477fb9c6668471fc52
ChepeCarlos <chepecarlos@alswblog.org>
2020-09-21 15:54:46 -0600

    Mejoras de codigo para la neurona para obtimizar

--- sketch.js
+++ sketch.js
@@ -5,16 +5,22 @@ var BotonesEntrenar;
 var knn;
 var modelo;
 var Clasificando = false;
+var CargandoNeurona = false;
+var RelacionCamara;
 
 function setup() {
   var ObtenerCanva = document.getElementById('micanva');
   CartaMensaje = document.getElementById('CartaMensaje');
   CartaMensaje.innerText = "Cargando APP...";
-  var Ancho = ObtenerCanva.offsetWidth;
-  var sketchCanvas = createCanvas(Ancho, 600);
-  sketchCanvas.parent("micanva");
+  var AnchoCanvas = ObtenerCanva.offsetWidth;
+
   Camara = createCapture(VIDEO);
+  // Camara.size(1280, 720);
   Camara.hide();
+  RelacionCamara = Camara.height / Camara.width;
+  var AltoCanvas = (AnchoCanvas * RelacionCamara);
+  var sketchCanvas = createCanvas(AnchoCanvas, AltoCanvas);
+  sketchCanvas.parent("micanva");
 
   modelo = ml5.featureExtractor('MobileNet', ModeloListo);
   knn = ml5.KNNClassifier();
@@ -38,20 +44,30 @@ function setup() {
 }
 
 function draw() {
+  background("#b2dfdb");
 
-  image(Camara, 0, 0, width, width * Camara.height / Camara.width);
+  image(Camara, 0, 0, width, height);
 
   if (knn.getNumLabels() > 0 && !Clasificando) {
     setInterval(clasificar, 500);
     Clasificando = true;
   }
 
+  var RelacionCamara2 = Camara.height / Camara.width;
+  if (RelacionCamara != RelacionCamara2) {
+    var Ancho = width;
+    var Alto = Ancho * RelacionCamara2;
+    RelacionCamara = RelacionCamara2;
+    console.log("Cambiando " + Ancho + " - " + Alto);
+    resizeCanvas(Ancho, Alto, true);
+  }
 }
 
 function windowResized() {
   var ObtenerCanva = document.getElementById('micanva');
   var Ancho = ObtenerCanva.offsetWidth;
-  resizeCanvas(Ancho, 450);
+  var Alto = Ancho * RelacionCamara;
+  resizeCanvas(Ancho, Alto);
 }
 
 function ModeloListo() {
@@ -66,36 +82,45 @@ function PresionandoBoton() {
 }
 
 function EntrenarKnn(ObjetoEntrenar) {
-  const Imagen = modelo.infer(Camara);
+  var Imagen = modelo.infer(Camara);
   knn.addExample(Imagen, ObjetoEntrenar);
 }
 
 
 function clasificar() {
   if (Clasificando) {
-    const Imagen = modelo.infer(Camara);
+    var Imagen = modelo.infer(Camara);
     knn.classify(Imagen, function(error, result) {
       if (error) {
+        console.log("Error en clasificar");
         console.error();
       } else {
-
-        Etiquetas = Object.keys(result.confidencesByLabel);
-        Valores = Object.values(result.confidencesByLabel);
-        var Indice = 0;
-
-        for (var i = 0; i < Valores.length; i++) {
-          if (Valores[i] > Valores[Indice]) {
-            Indice = i;
+        console.log(result);
+        var Etiqueta;
+        var Confianza;
+        if (!CargandoNeurona) {
+          Etiqueta = result.label;
+          Confianza = Math.ceil(result.confidencesByLabel[result.label] * 100);
+        } else {
+          Etiquetas = Object.keys(result.confidencesByLabel);
+          Valores = Object.values(result.confidencesByLabel);
+          var Indice = 0;
+          for (var i = 0; i < Valores.length; i++) {
+            if (Valores[i] > Valores[Indice]) {
+              Indice = i;
+            }
           }
+          Etiqueta = Etiquetas[Indice];
+          Confianza = Math.ceil(Valores[Indice] * 100);
         }
-        CartaMensaje.innerText = Etiquetas[Indice] + " - " + (Math.ceil(Valores[Indice] * 100)) + "%";
+        CartaMensaje.innerText = Etiqueta + " - " + Confianza + "%";
       }
-    })
+    });
   }
 }
 
 function EntrenarTexBox() {
-  const Imagen = modelo.infer(Camara);
+  var Imagen = modelo.infer(Camara);
   var EtiquetaTextBox = select("#TextBox").value();
   knn.addExample(Imagen, EtiquetaTextBox);
 }
@@ -112,13 +137,16 @@ function CargarNeurona() {
   knn.load("./data/NeuronaKNN.json", function() {
     console.log("Neurona Cargada knn");
     CartaMensaje.innerText = "Neurona cargana de archivo";
-  })
+    CargandoNeurona = true;
+  });
 }
 
 function LimpiarKnn() {
-  console.log("Borrando Neuroona")
-  knn.clearAllLabels();
-  Clasificando = false;
-  CartaMensaje.innerText = "Neurona Borrada";
-  clearInterval(clasificar);
+  console.log("Borrando Neuroona");
+  if (Clasificando) {
+    Clasificando = false;
+    clearInterval(clasificar);
+    knn.clearAllLabels();
+    CartaMensaje.innerText = "Neurona Borrada";
+  }
 }
